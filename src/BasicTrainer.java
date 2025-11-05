e5db66c1349761ff05f4f941cebb94f4a38af231
calciumking <lkingerslev@gmail.com>
2025-11-05 00:10:20 -0500

    Formatting, expanding input and output values, converting List<> to ArrayList<>.

--- src/BasicTrainer.java
+++ src/BasicTrainer.java
@@ -1,32 +1,37 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class BasicTrainer {
-	float[][] inputs = new float[][]{new float[]{0, 0}, new float[]{1, 0}, new float[]{0, 1}, new float[]{1, 1}};
-	int[] outputIndexes = new int[]{0, 1, 1, 1};
+	private final float[][] inputs = new float[][] {
+			{0, 0, 0, 0, 0},
+			{1, 0, 0, 0, 0},
+			{1, 1, 1, 1, 1}
+	};
 	
-	List<NeuralNetwork> agents = new ArrayList<>();
+	private final int[] outputs = new int[] {
+			0, 1, 1
+	};
+	
+	
+	private ArrayList<NeuralNetwork> agents = new ArrayList<>();
 	
 	
 	public void train() {
-		int agent_per_round = 100;
-		int number_of_rounds = 400;
+		int agent_per_round = 5;
+		int number_of_rounds = 25;
 		
-		for (int i = 0; i < agent_per_round; i++){
-			agents.add(new NeuralNetwork(new int[]{2, 2}));
-		}
+		for (int i = 0; i < agent_per_round; i++)
+			agents.add(new NeuralNetwork(new int[] { inputs[0].length, 2 }));  // new network with two layers of two nodes
 		
 		System.out.println("Starting Training");
 		
-		for (int i = 0; i < number_of_rounds; i++){
-			int best_score = -1;
-			NeuralNetwork best_agent = null;
-			
-			for (NeuralNetwork agent : agents){
+		int best_score = -1;
+		NeuralNetwork best_agent = agents.getFirst();
+		for (int i = 1; i <= number_of_rounds; i++) {
+			for (NeuralNetwork agent : agents) {
 				int score = eval(agent);
 				
-				if (score > best_score){
+				if (score > best_score) {
 					best_score = score;
 					best_agent = agent;
 				}
@@ -35,9 +40,7 @@ public class BasicTrainer {
 			System.out.println("Round " + i + " Best score " + best_score);
 			
 			agents = new ArrayList<>();
-			for (int j = 0; j < agent_per_round; j++){
-				assert best_agent != null;
-				
+			for (int j = 0; j < agent_per_round; j++) {
 				agents.add(best_agent.evolve(0.2f));
 			}
 		}
@@ -47,16 +50,14 @@ public class BasicTrainer {
 	public int eval(NeuralNetwork network) {
 		int score = 0;
 		for (int i = 0; i < inputs.length; i++) {
-			List<Float> calculatedOutputs = network.calculate(inputs[i]);
+			ArrayList<Float> calculatedOutputs = network.calculate(inputs[i]);
 			
 			Optional<Float> max = calculatedOutputs.stream().max(Float::compareTo);
-			if (max.isEmpty()){
+			if (max.isEmpty())
 				continue;
-			}
 			
-			if (outputIndexes[i] == calculatedOutputs.indexOf(max.get())){
+			if (outputs[i] == calculatedOutputs.indexOf(max.get()))
 				score++;
-			}
 		}
 		
 		return score;
