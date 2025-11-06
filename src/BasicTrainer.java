dbfa6da2cd03368e6cda4781ff157a2ee8e9d3aa
calciumking <lkingerslev@gmail.com>
2025-11-06 00:57:10 -0500

    Agent evaluation multithreading:
    - Changed best_score to AtomicInteger and best_agent to AtomicReference so threads can easily access and edit them without overlap

--- src/BasicTrainer.java
+++ src/BasicTrainer.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class BasicTrainer {
-	public void train(float[][] inputs, int[] outputs, int[] layers, int agents_per_round, int number_of_rounds) {
+	public void train(float[][] inputs, int[] outputs, int[] layers,
+	                  int agents_per_round, int number_of_rounds) throws InterruptedException {
 		ArrayList<NeuralNetwork> agents = new ArrayList<>();
 		
 		for (int i = 0; i < agents_per_round; i++)
@@ -11,24 +14,33 @@ public class BasicTrainer {
 		int max_score = outputs.length;
 		System.out.println("Starting Training");
 		
-		int best_score = -1;
-		NeuralNetwork best_agent = agents.getFirst();
+		AtomicInteger best_score = new AtomicInteger(-1);
+		AtomicReference<NeuralNetwork> best_agent = new AtomicReference<>(agents.getFirst());
+		ArrayList<Thread> threads = new ArrayList<>();
 		for (int i = 1; i <= number_of_rounds; i++) {
 			for (NeuralNetwork agent : agents) {
-				int score = eval(agent, inputs, outputs);
-				
-				if (score > best_score) {
-					best_score = score;
-					best_agent = agent;
-				}
+				Thread thread = new Thread(() -> {
+					int score = eval(agent, inputs, outputs);
+					
+					if (score > best_score.get()) {
+						best_score.set(score);
+						best_agent.set(agent);
+					}
+				});
+				thread.start();
+				threads.add(thread);
 			}
 			
-			float percent = ((float) best_score / max_score) * 100;
+			for (Thread thread : threads)
+				thread.join();
+			threads.clear();
+			
+			float percent = ((float) best_score.get() / max_score) * 100;
 			System.out.println("Round: " + i + " Best score: " + best_score + " Which is: " + percent + "%");
 			
 			agents = new ArrayList<>();
 			for (int j = 0; j < agents_per_round; j++)
-				agents.add(best_agent.evolve(0.2f));
+				agents.add(best_agent.get().evolve(0.2f));
 		}
 	}
 	
