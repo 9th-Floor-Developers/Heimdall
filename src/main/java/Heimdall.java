8e36c12fd4144dab33ab74c68632dc41206b8430
petepoop <pete.sucharitkul@gmail.com>
2026-08-14 21:07:30 +0700

    Added an explanation for DataSet in entry point

--- src/main/java/Heimdall.java
+++ src/main/java/Heimdall.java
@@ -1,7 +1,9 @@
+import core.data.CustomDataSetLoader;
 import core.data.DataSet;
 import core.trainers.FeedForwardTrainer;
 import numberrecognizer.NumberImageLoader;
 
+import java.util.List;
 
 
 public class Heimdall {
@@ -13,11 +15,36 @@ public class Heimdall {
 	}
 	
 	public static void numberTrain() throws Exception {
+		/*
+		To create a dataset, extend the AbstractDataSetLoader class
+		AbstractDataSetLoader provides ability to adjust the dataset used in training and testing
+
+		Here is an example using NumberImageLoader a loader from our number recognizer library:
+		 */
 		DataSet dataSet = NumberImageLoader.createLoader()
 				.setSrc("./src/main/resources/numbers/")
 				.setTrainingSize(10000)
 				.setTestingSize(500)
 				.load();
+		/*
+		Alternate example
+		Loads 20K training data point and use remaining for testing:
+
+		DataSet dataSet = NumberImageLoader.createLoader()
+				.setSrc("./src/main/resources/numbers/")
+				.setTrainingSize(20000)
+				.setTestingSizeAsRemaining()
+				.load();
+
+		Quick way to create a new dataset:
+
+		DataSet dataSet = CustomDataSetLoader.loadFromList(List.of(...))
+				.setSrc("./src/main/resources/numbers/")
+				.setTrainingSize(100)
+				.setTestingSize(50)
+				.load();
+		 */
+
 
 		FeedForwardTrainer feedForwardTrainer = (FeedForwardTrainer) new FeedForwardTrainer(
 			// number of agents per round, more possibilities to evolve
